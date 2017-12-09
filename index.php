b7d924e73be4684de0735e7582e2563c216fabda
Jeremy MARTIN <cerynna@gmail.com>
2017-12-09 18:03:48 +0100

    :sunny:

--- index.php
+++ index.php
@@ -1,15 +1,28 @@
 <?php
 function sendMessage($parameters) {
-    header('Content-Type: application/json');
-    echo json_encode($parameters);
+
+
 }
+header('Content-Type: application/json');
+$json = '{
+  "fulfillmentText": string,
+  "fulfillmentMessages": [
+    {
+      object(Message)
+    }
+  ],
+  "source": string,
+  "payload": {
+    object
+  },
+  "outputContexts": [
+    {
+      object(Context)
+    }
+  ],
+  "followupEventInput": {
+    object(EventInput)
+  },
+}';
+echo $json;
 
-$parameters = array(
-    "speech" => "Here are the results : None.Please say Detail Placename for more details of a place.",
-    "displayText" => "Hello Google",
-    "contextOut" => "",
-    "source" => "agent"
-);
-
-
-sendMessage($parameters);

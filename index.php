24d079d943117f4fdc60da29ab87bc4896a5baec
Jeremy MARTIN <cerynna@gmail.com>
2017-12-09 18:27:27 +0100

    :sunny:

--- index.php
+++ index.php
@@ -7,8 +7,6 @@ function sendMessage($parameters) {
 $animals = [
 
         "name" => "lièvre",
-        "female" => "hase (qui s'écrit <say-as interpret-as=\"characters\">HASE</say-as>)",
-        "child" => "levraut (qui s'écrit <say-as interpret-as=\"characters\">LEVRAUT</say-as>)",
         "sound" => [
             "noun" => "vagissement",
             "verb" => "vagit"

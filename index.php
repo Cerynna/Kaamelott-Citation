7039ab8143fa21a56234976563fdd49212acc552
Jeremy MARTIN <cerynna@gmail.com>
2017-12-09 22:33:02 +0100

    :sunny:

--- index.php
+++ index.php
@@ -1,12 +1,16 @@
 <?php
-function sendMessage($parameters) {
-    header('Content-Type: application/json');
-    echo json_encode($parameters);
+require_once __DIR__.'/vendor/autoload.php';
+
+use DialogFlow\Client;
+
+try {
+$client = new Client('access_token');
+
+$query = $client->get('query', [
+'query' => 'Hello',
+]);
+
+$response = json_decode((string) $query->getBody(), true);
+} catch (\Exception $error) {
+echo $error->getMessage();
 }
-
-$animals = [
-        "name" => "lièvre",
-];
-
-
-sendMessage($animals);

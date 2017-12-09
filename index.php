33d3020313bfde6366bff833eff98090a568bc5e
Jeremy MARTIN <cerynna@gmail.com>
2017-12-09 18:00:02 +0100

    :sunny:

--- index.php
+++ index.php
@@ -1,8 +1,15 @@
 <?php
+function sendMessage($parameters) {
+    header('Content-Type: application/json');
+    echo json_encode($parameters);
+}
 
-$data = [
-    'speech' => 'Perceval',
-    'displayText' => 'LOL'
-];
-header('Content-Type', 'application/json; charset=UTF-8');
-echo json_encode($data);
+$parameters = array(
+    "speech" => "Here are the results : None.Please say Detail Placename for more details of a place.",
+    "displayText" => "Hello Google",
+    "contextOut" => "",
+    "source" => "agent"
+);
+
+
+sendMessage($parameters);

28399104b6b9471c4c1b49bca00d9d7a30b70ae4
Jeremy MARTIN <cerynna@gmail.com>
2017-12-10 23:59:01 +0100

    :sunny:

--- index.php
+++ index.php
@@ -8,7 +8,6 @@ require __DIR__ . '/vendor/autoload.php';
 require "MyBot.php";
 
 
-
 $method = $_SERVER['REQUEST_METHOD'];
 
 // Process only when method is POST
@@ -16,9 +15,12 @@ if ($method == 'POST') {
     $requestBody = file_get_contents('php://input');
     $json = json_decode($requestBody);
 
+
     $personnage = strtolower($json->result->parameters->personnage);
     $allQuery = strtolower($json->result->resolvedQuery);
     $action = strtolower($json->result->parameters->action);
+    $list = strtolower($json->result->parameters->list);
+
     $myBot = new MyBot();
 
     if ( !empty($action)  and $action === "ajouter")
@@ -29,12 +31,13 @@ if ($method == 'POST') {
         $myBot->addCitation($personnage,$allQuery, $citation);
         $speech = $citation;
     }
+    elseif(!empty($list))
+    {
+        $speech = "Listing";
+    }
     else {
-
         $myBot->getCitation($personnage, $citation);
         $speech = $citation;
-        $destinationName = "test";
-        $url= "www.google.fr";
     }
 
 
@@ -42,10 +45,6 @@ if ($method == 'POST') {
     $response = new \stdClass();
     $response->speech = $speech;
     $response->displayText = $speech;
-
-    $response->url = $url;
-    $response->destinationName = $destinationName;
-
     $response->source = "webhook";
     echo json_encode($response);
 } else {
